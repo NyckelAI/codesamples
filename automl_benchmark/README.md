8da48ccf8cf8954579315094d647abd24f339857
Oscar Beijbom <oscar.beijbom@gmail.com>
2021-06-08 16:30:50 -0700

    Adding automl benchmark code

---
+++ automl_benchmark/README.md
@@ -0,0 +1,141 @@
+# Code samples for AutoML blogpost
+
+TODO: insert link to blogpost.
+
+## Setup
+
+NOTE: This code is only tested on `python3.9`
+
+### Create python environment
+
+* Install `python3.9`.
+* Clone this repository.
+* Go to `automl_benchmark` folder.
+* Create a python virtual environment. For example say:
+
+```bash
+python3.9 -m venv automl_benchmark_env
+source automl_benchmark_env/bin/activate
+```
+
+* Install requirements like so `pip install -r requirements.txt`.
+* Install autonlp like so: `pip install autonlp==0.2.6 --no-dependencies`.
+
+### Prepare data files
+
+To download the data files and prepare `.csv` files run `prepare_data.py` like so
+
+```bash
+python prepare_data.py --dataset_name imdb --n_train 500
+```
+
+This generates five `.csv` file in this folder.
+
+```bash
+Filename                    | Details
+---------------------------------------------------------------------
+imdb_train_500.csv          | train data (400 samples)
+imdb_val_500.csv            | validation data (100 samples)
+imdb_trainval_500.csv       | train and validation data (500 samples)
+imdb_test.csv               | test data (1000 samples)
+imdb_all_500.csv            | train, val and test data (1500 samples)
+```
+
+All `.csv` files are formatted like so:
+
+```bash
+split_name, text, label_name
+```
+
+## Nyckel
+
+### Training using the UI
+
+Create a new function at nyckel.com and follow the instructions. For the `IMDB` dataset name your labels `pos` and `neg`. Like so:
+
+![image](instruction_images/nyckel_define_page.png)
+
+At the import step choose `imdb_trainval_500.csv` and select columns corresponding to text and labels.
+
+### Training using the API
+
+To train using the API first setup a function using the UI as instructed above. Then navigate to the `API` page of the function you just created and copy the `client_id` and `client_secret`. Create environment variables like so:
+
+```bash
+export NYCKEL_CLIENT_ID=<Your client ID>
+export NYCKEL_CLIENT_SECRET=<Your client secret>
+```
+
+Then run
+
+```bash
+python nyckel.py train imdb 500
+```
+
+to create a new function using the API. Copy the function id printed by the console.
+
+### Evaluate performance
+
+Setup your environment variables like above, then run
+
+```bash
+python nyckel.py evaluate <your_function_id> imdb_test.csv
+```
+
+## Huggingface
+
+### Training using the CLI
+
+Create a Huggingface AutoNLP account. Copy API key to environment varilable like so
+
+```bash
+export HF_API_KEY=<your API key>
+```
+
+To train run
+
+```bash
+python huggingface.py train imdb 500
+```
+
+You will have to accept the ~$15 charge. Then run
+
+```bash
+python huggingface.py monitor imdb_500
+```
+
+to check the progress of your training. Once complete select the model which does best on the validation set.
+
+### Evaluate performance
+
+Run
+
+```bash
+python huggingface.py evaluate imdb_500 <your_selected_model_id> imdb_test.csv
+```
+
+to evaluate your selected model on the imdb test set.
+
+## Google Vertex AI
+
+### Train a model
+
+We use the Google GCP UI to train the models. The steps are as follows:
+
+* Use the Google GCP console to create an account.
+* Create a new dataset and select the `imdb_all_500.csv` file.
+* Train a model based on the selected dataset. Use the "Manual (advanced) option for data splits".
+
+![image](instruction_images/gcp_train.png)
+
+### Evaluate performance
+
+Since we upload the test data we can read out the test set performance from the console. Simply add the CM diagonal and divide by 1000. In this example: `(438 + 443) / 1000 = 88.1%`.
+
+![image](instruction_images/gcp_stats.png)
+
+Alternatively, create a deploy endpoint using the UI, set `GCP_BEARER_TOKEN` and `GCP_INVOKE_ENDPOINT` env. variables and run.
+
+```bash
+python google.py evaluate imdb_test.csv
+```
---
+++ automl_benchmark/nyckel_utils/function.py
@@ -0,0 +1,129 @@
+import multiprocessing
+import time
+from typing import List
+
+import requests
+from joblib import Parallel, delayed
+
+from .sample import Modality, Sample
+from .requester import Requester
+
+
+class FunctionBuilder:
+    def __init__(
+        self,
+        name: str,
+        input_modality: Modality,
+        requester: Requester,
+        project_id: str = None,
+        description: str = "",
+        is_public: bool = False,
+    ):
+
+        self.requester = requester
+
+        if not project_id:
+            resp = self.requester(requests.get, "projects").json()
+            if len(resp) < 1:
+                project_id = self._post_project()
+                print(f"No project found for this account. Project id {project_id} created.")
+            elif len(resp) == 1:
+                project_id = resp[0]["id"]
+            else:
+                project_id = resp[0]["id"]
+                print(f"Found more than 1 project associated with the credentials. Using {project_id}.")
+
+        self.function_id = self._post_function(name, input_modality, description, is_public, project_id)
+        self.local_id_to_server_id = dict()
+        self.server_id_to_local_id = dict()
+        self.annotated_sample_ids = set()
+        self.label_name_to_id = dict()
+        print(f"Initialized {self.requester.host}console/functions/{self.function_id}/browse")
+
+    def add_label(self, name: str, description: str = ""):
+        label_id = self.requester(
+            requests.post, f"functions/{self.function_id}/labels", json={"name": name, "description": description}
+        ).json()["id"]
+        self.label_name_to_id[name] = label_id
+
+    def add_samples(self, samples: List[Sample]):
+        cpu_count = multiprocessing.cpu_count()
+        sample_ids = Parallel(n_jobs=cpu_count, prefer="threads")(
+            delayed(self._post_sample)(sample) for sample in samples
+        )
+        for sample, server_sample_id in zip(samples, sample_ids):
+            self.local_id_to_server_id[sample.id] = server_sample_id
+            self.server_id_to_local_id[server_sample_id] = sample.id
+
+    def add_annotations(self, samples: List[Sample]):
+        cpu_count = multiprocessing.cpu_count()
+        Parallel(n_jobs=cpu_count, prefer="threads")(delayed(self._post_annotation)(sample) for sample in samples)
+        self.annotated_sample_ids |= set([sample.id for sample in samples])
+
+    @property
+    def nbr_samples(self):
+        return len(self.local_id_to_server_id.keys())
+
+    @property
+    def nbr_annotations(self):
+        return len(self.annotated_sample_ids)
+
+    def has_model(self):
+        resp = self.requester(requests.get, f"functions/{self.function_id}/models")
+        for model_dict in resp.json():
+            if model_dict["trainingPercentage"] == 100:
+                return True
+        return False
+
+    def sleep_until_has_at_least_one_model(self, poll_interval_sec=1):
+        while not self.has_model():
+            time.sleep(poll_interval_sec)
+
+    def _post_sample(self, sample):
+        if sample.modality.name == "Text":
+            inline_data = sample.data
+        elif sample.modality.name == "Image":
+            inline_data = "data:image/png;base64," + sample.data
+        else:
+            raise ValueError(f"Unknown modality: {sample.modality}")
+
+        return self.requester(
+            requests.post,
+            f"functions/{self.function_id}/samples",
+            json={
+                "externalId": sample.id,
+                "input": {"modality": sample.modality.name, "inlineData": inline_data, "referenceUrl": ""},
+            },
+        ).json()["id"]
+
+    def _post_annotation(self, sample):
+        assert sample.id in self.local_id_to_server_id, f"Need to post sample first for id {sample.id}"
+        self.requester(
+            requests.post,
+            f"functions/{self.function_id}/annotations",
+            json={
+                "sampleId": self.local_id_to_server_id[sample.id],
+                "labelId": self.label_name_to_id[sample.label_name],
+                "source": "User",
+            },
+        )
+
+    def _post_function(self, name, input_modality, description, is_public, project_id):
+        function_id = self.requester(
+            requests.post,
+            "functions",
+            json={
+                "projectId": project_id,
+                "name": name,
+                "inputModality": input_modality.name,
+                "description": description,
+                "isPublic": is_public,
+            },
+        ).json()["id"]
+        return function_id
+
+    def _post_project(self):
+        project_id = self.requester(requests.post, "projects", json={"name": "Auto-generated", "admins": []}).json()[
+            "id"
+        ]
+        return project_id
