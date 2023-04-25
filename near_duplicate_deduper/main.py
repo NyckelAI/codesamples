be08c7e808330405e5958df9867cf033505d2e16
Oscar Beijbom <oscar@nyckel.com>
2023-04-25 10:32:03 -0700

    python CLI that uses Nyckel Image Search functions to do near-deduplication of a set of images

---
+++ near_duplicate_deduper/main.py
@@ -0,0 +1,31 @@
+import glob
+import os
+from typing import List, Set
+
+import fire
+import time
+
+from near_duplicate_deduper import NyckelNearDuplicateDeduper
+
+COMMON_IMAGE_EXTENSIONS = ["jpg", "png", "jpeg", "JPG", "JPEG", "PNG"]
+
+
+def main(client_id: str, client_secret: str, folder: str, max_nbr_file_to_dedupe: int = 10):
+    image_filelist = []
+    for image_extension in COMMON_IMAGE_EXTENSIONS:
+        image_filelist.extend(glob.glob(os.path.join(folder, f"*.{image_extension}")))
+    image_filelist = image_filelist[:max_nbr_file_to_dedupe]
+
+    t0 = time.time()
+    duplicate_clusters = NyckelNearDuplicateDeduper(client_id, client_secret).dedupe_filelist(image_filelist)
+    runtime = time.time() - t0
+
+    print("----")
+    print(f"Deduped {len(image_filelist)} in {runtime} seconds.")
+    print(f"Found {len(duplicate_clusters)} clusters of duplicates")
+    for cluster in duplicate_clusters:
+        print(f"Cluster1: {cluster}")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
